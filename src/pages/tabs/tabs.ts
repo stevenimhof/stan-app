517d659b79ee94018fb1aee59b3161e0bc69a3eb
Alexander Nakamura <nakamale@students.zhaw.ch>
2018-03-13 13:13:04 +0100

    added infoPage

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 import {HomePage} from '../home/home';
 import {ExercisesPage} from "../exercises/exercises";
 import {TheoriesPage} from "../theories/theories";
-import {SettingsPage} from "../settings/settings";
+import {InfoPage} from "../info/info";
 
 @Component({
   templateUrl: 'tabs.html'
@@ -13,7 +13,7 @@ export class TabsPage {
   tab1Root = HomePage;
   tab2Root = ExercisesPage;
   tab3Root = TheoriesPage;
-  tab4Root = SettingsPage;
+  tab4Root = InfoPage;
 
   constructor() {
 
