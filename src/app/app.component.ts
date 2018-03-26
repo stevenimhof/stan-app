859c9a66eeaf4634ecaf1908e85be99cf0b40655
Steven Imhof <steven.imhof@hotmail.ch>
2018-03-26 13:27:26 +0200

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,7 @@ export class MyApp {
       splashScreen.hide();
 
       this.checkForExerciseUpdates();
-      this.checkForMotivationUpdates();
+      this.motivationProvider.checkForUpdates();
     });
   }
 
@@ -36,7 +36,7 @@ export class MyApp {
     this.storage.get('exercises').then((localExercises) => {
       this.exerciseProvider.getCategoriesFromWordpress()
       .subscribe(result => {
-        console.log(result);
+        //console.log(result);
         const categories = result;
 
         this.exerciseProvider.getExercisesFromWordpress()
@@ -44,14 +44,14 @@ export class MyApp {
           const exercises = result;
 
           if (localExercises === null || JSON.stringify(localExercises['exercises']) != JSON.stringify(exercises) || JSON.stringify(localExercises['categories']) != JSON.stringify(categories)) {
-            console.log('updated exercise data in storage');
+            //console.log('updated exercise data in storage');
             this.storage.set('exercises', {
               "exercises" : exercises,
               "categories" : categories,
               "evaluations" : []
             });
           } else {
-            console.log('no new data from rest');
+            //console.log('no new data from rest');
           }
         },
         err => {
@@ -62,30 +62,5 @@ export class MyApp {
         console.log(err);
       });
     });
-  }
-
-  checkForMotivationUpdates() {
-    this.storage.get('motivations').then((localMotivations) => {
-      this.motivationProvider.getMotivationsFromWordpress()
-      .subscribe(result => {
-        console.log('motivations: ' + result);
-        const motivations = result;
-
-        if (localMotivations === null || JSON.stringify(localMotivations['motivations']) != JSON.stringify(motivations)) {
-          console.log('updated motivations in local storage');
-          this.storage.set('motivations', {
-            'motivations': motivations
-          });
-        } else {
-          console.log('motivations up do date')
-        }
-        },
-        error => {
-          console.log(error);
-        });
-    },
-  error => {
-    console.log(error);
-  });
   }
 }
